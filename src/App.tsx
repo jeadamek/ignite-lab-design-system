d49fc88d9cb68816d18fc944471e1242496f70e8
jessica <jessica.g.adamek@gmail.com>
2022-10-15 19:18:05 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className='text-black text-2xl'>Hello world!</h1>
+      <button className='bg-cyan-500'>hello</button>
+    
+    </>
+  )
+}
